f6cbeb986852d218f2828a1464bff4a4fb921815
TiagoMerc <tmrrosario9@gmail.com>
2022-06-13 22:19:37 -0300

    Arrays, Loop e Estilos

--- README.md
+++ README.md
@@ -31,3 +31,19 @@ Neste capítulo vimos:
  - Busca de elementos
 
  ![Print das Variaveis e Operadores](img/print/print2.png)
+
+ ### 03. Arrays, Loop e Estilos
+
+ Neste capítulo aprendemos a lidar uma coleção de elementos do HTML e vimos:
+
+- Como selecionar diversos elementos com a função querySelectorAll()
+
+- Relembramos um método clássico de iteração com o for
+
+- Vimos como manipular estilos com a propriedade .style
+
+- Vimos que devemos utilizar camelCase quando queremos modificar uma propriedade que tenha duas palavras, como a background-color
+
+- A boa prática de não alterar o estilo diretamente no Javascript e sim modificar as classes dos elementos com a propriedade .classList
+
+ ![Print Arrays, Loops e Estilos](img/print/print3.png)
--- js/principal.js
+++ js/principal.js
@@ -1,47 +1,57 @@
 /* 	console.log(document.querySelector("h1")); */
 var titulo = document.querySelector(".titulo");
-console.log(titulo);
+/* console.log(titulo);
 
-console.log(titulo.textContent);
+console.log(titulo.textContent); */
 titulo.textContent = "Aparecida nutricionista";
 
-console.log("Fui carregado de um arquivo externo");
+/* console.log("Fui carregado de um arquivo externo");
+ */
+var pacientes = document.querySelectorAll(".paciente");
 
-var paciente = document.querySelector("#tabela-pacientes");
+for(var i = 0; i < pacientes.length; i++){
 
-var tdPeso = document.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+  var paciente = pacientes[i];
 
-var tdAltura = document.querySelector(".info-altura");
-var altura = tdAltura.textContent;
-
-var tdInfo = document.querySelector(".info-imc");
-
-console.log(paciente); //tr
-console.log(tdPeso); // td que tem o peso
-console.log(peso); //obter 100
-
-console.log(tdAltura);
-console.log(altura);
-
-
-var pesoEhValido = true;
-var alturaEhValida = true;
-
-
-if(peso <= 0 || peso >= 1000) {
-   tdInfo.textContent = "Peso invalido!"
-   pesoEhValido = false;
-}
-
-if(altura <= 0 || altura >= 3.000) {
-  tdInfo.textContent = "Altura Inválida!"
-  alturaEhValida = false; 
-}
-
-if(alturaEhValida && pesoEhValido){
-  var IMC = peso/(altura*altura);
-  console.log(IMC);
-  tdInfo.textContent = IMC; // Colocando o resultado o IMC do Paulo na posição correta na coluna IMC
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
   
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
+  
+  var tdImc = paciente.querySelector(".info-imc");
+  
+  console.log(paciente); //tr
+  console.log(tdPeso); // td que tem o peso
+  console.log(peso); //obter 100
+  
+  console.log(tdAltura);
+  console.log(altura);
+  
+  var pesoEhValido = true;
+  var alturaEhValida = true;
+  
+  if(peso <= 0 || peso >= 1000) {
+     tdImc.textContent = "Peso invalido!"
+     pesoEhValido = false;
+    /*  paciente.style.backgroundColor="orange"; */
+     paciente.classList.add("paciente-invalido");
+  }
+  
+  if(altura <= 0 || altura >= 3.000) {
+    tdImc.textContent = "Altura Inválida!"
+    alturaEhValida = false;
+   /*  paciente.style.backgroundColor="orange";  */
+    paciente.classList.add("paciente-invalido");
+  }
+  
+  if(alturaEhValida && pesoEhValido){
+    var IMC = peso/(altura*altura);
+    console.log(IMC);
+    tdImc.textContent = IMC.toFixed(2); // Colocando o resultado o IMC do Paulo na posição correta na coluna IMC
+    
+  }
+
 }
+
+
