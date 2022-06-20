5a3a01d0594f6fa4d87f8b84000c1bd65081c782
TiagoMerc <tmrrosario9@gmail.com>
2022-06-20 16:54:27 -0300

    Atualizando o link da requisição

--- js/buscar-pacientes.js
+++ js/buscar-pacientes.js
@@ -5,7 +5,7 @@ botaoAdicionar.addEventListener("click", function(){
 
   var xhr = new XMLHttpRequest();
 
-  xhr.open("GET", "https://api-pacientes.herokuapp.com/paci1entes");
+  xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientes");
 
   xhr.addEventListener("load", function(){
     var erroAjax = document.querySelector("#erro-ajax");
